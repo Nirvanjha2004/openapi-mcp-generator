34b1425d0dcace4a1c4818f30559826cc5c6628c
Nirvanjha2004 <me23b1079@iiitdm.ac.in>
2026-09-03 14:06:39 +0530

    chore: initialize project scaffold with package manifests and tooling

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "no-console": "off",
+  },
+  ignorePatterns: ["dist", "node_modules", "*.js"],
+};
---
+++ tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+    "cli/index": "src/cli/index.ts",
+  },
+  format: ["esm", "cjs"],
+  dts: true,
+  clean: true,
+  splitting: false,
+  sourcemap: true,
+  target: "node18",
+  shims: true,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+  esbuildOptions(options) {
+    options.banner = {
+      js: '#!/usr/bin/env node',
+    };
+  },
+});
